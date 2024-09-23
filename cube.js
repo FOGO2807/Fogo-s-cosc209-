733ed5f04e6a446dd75791bebede43f19725a159
Mafe Fogo <mafefogo20@gmail.com>
2024-09-23 16:33:16 +0100

    first commit

---
+++ cube.js
@@ -0,0 +1,17 @@
+// Function to calculate the area of a rectangle
+function rectangleArea(length, width) {
+    return length * width;
+  }
+  
+  // Function to calculate the cube of a number
+  function cube(number) {
+    return number ** 3;
+  }
+  
+  // Calculate the area of a rectangle with length 5 and width 15
+  console.log(`The area of the rectangle is ${rectangleArea(5, 15)}`);
+  
+  // Calculate the cube of numbers 5, 15, and 20
+  console.log(`The cube of 5 is ${cube(5)}`);
+  console.log(`The cube of 15 is ${cube(15)}`);
+  console.log(`The cube of 20 is ${cube(20)}`);
---
+++ exam.js
@@ -0,0 +1,50 @@
+//Example of Classes
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    sayHello() {
+      console.log("Hello, my name is " + this.name);
+    }
+  }
+  
+  let john = new Person("John");
+  john.sayHello();
+  
+  //Example of Variables
+  let name = 'John';
+  console.log(name);  
+  name = 'Fogo'; 
+  console.log(name); 
+  
+  //Example of Array Methods
+  let fruit = ['apple', 'banana', 'orange'];
+  fruit.push('grape', 'mango');
+  console.log(fruit);
+  
+  //Example of Destructuring
+  let fruits = ['apple', 'banana', 'orange'];
+  let [fruit1, fruit2, fruit3] = fruits;
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+  
+  //Examples of Modules
+  // math.js
+  export function add(a, b) {
+      return a + b;
+    }
+    export function subtract(a, b) {
+      return a - b;
+    }
+  
+  //Example of Ternary Operator
+  let isAdmin = true;
+  let message = isAdmin ? 'You are an Admin' : 'You are not an Admin';
+  console.log(message);
+  
+  //Example of Spread Operator
+  let arr1 = [1, 2, 3];
+  let arr2 = [...arr1, 4, 5, 6];
+  console.log(arr2); 
